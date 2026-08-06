4b65cfe2d5f21968abb074f0d109e5cc7f35d9ca
Ljuben Angelkoski <ljubenangelkoski_pp1@outlook.com>
2026-08-06 17:28:00 +0200

    Version 0.0.43

--- Package.swift
+++ Package.swift
@@ -1,29 +1,36 @@
 // swift-tools-version: 6.3
-
 import PackageDescription
 
 let package = Package(
     name: "ESimSDK",
     products: [
-        .library(
-            name: "ESimSDK",
-            targets: ["ESimSDK"]
-        ),
-        .library(
-            name: "ESimShared",
-            targets: ["ESimShared"]
-        )
+        .library(name: "ESimSDK", targets: ["ESimSDKKit"]),
+        .library(name: "ESimShared", targets: ["ESimShared"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/zendesk/sdk_messaging_ios", from: "2.25.0"),
+        .package(url: "https://github.com/zendesk/sdk_zendesk_ios", from: "3.3.0")
     ],
     targets: [
         .binaryTarget(
-            name: "ESimSDK",
-            url: "https://repo.netcetera.com/nexus/repository/internal-repository-release-ios/com/netcetera/esim/ESimSDK/0.0.22/ESimSDK-0.0.22-xcframework.zip",
-            checksum: "4a12afb0192f1d1b2f3586880edf4673de785748722dc2a5a04e71b721400071"
+            name: "ESimSDKBinary",
+            url: "https://repo.netcetera.com/nexus/repository/internal-repository-release-ios/com/netcetera/esim/ESimSDK/0.0.43/ESimSDK-0.0.43-xcframework.zip",
+            checksum: "ad63993b2c1c6765312efd561e6d11abd838e1995ec420390f7044b4a547ffd6"
         ),
         .binaryTarget(
             name: "ESimShared",
-            url: "https://repo.netcetera.com/nexus/repository/internal-repository-release/com/netcetera/esim/esim-shared-sdk-ios/0.0.22/esim-shared-sdk-ios-0.0.22-xcframework.zip",
-            checksum: "752171e74582af51e9a08d2ceff635b1227db8de9c85f41656a5ab52c534df59"
+            url: "https://repo.netcetera.com/nexus/repository/internal-repository-release/com/netcetera/esim/esim-shared-sdk-ios/0.0.43/esim-shared-sdk-ios-0.0.43-xcframework.zip",
+            checksum: "11edc22c2374c44c37ee12b0e5e1c7e8848900c5e6e5e5145bdee55992bf7f56"
+        ),
+        .target(
+            name: "ESimSDKKit",
+            dependencies: [
+                "ESimSDKBinary",
+                "ESimShared",
+                .product(name: "ZendeskSDKMessaging", package: "sdk_messaging_ios"),
+                .product(name: "ZendeskSDK", package: "sdk_zendesk_ios")
+            ],
+            path: "Sources/ESimSDKKit"
         )
     ],
     swiftLanguageModes: [.v6]
